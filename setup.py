e20b2e85568562711f1aa0fe334d09058dffc0fb
Infqq <57950063+Infqq@users.noreply.github.com>
2020-11-20 21:39:49 +0400

    ok

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="simpledemotivators",
+    version="1.0.0",
+    author="infqq",
+    description="Tool for Rates",
+    url="https://github.com/Infqq/simpledemotivators",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "PIL~=7.1.1"
+    ]
+)
