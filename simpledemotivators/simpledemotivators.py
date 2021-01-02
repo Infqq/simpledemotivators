b17538f9973682ec9416d412fe4a7f686234cdae
Aleksandr <57950063+Infqq@users.noreply.github.com>
2021-01-02 18:56:51 +0400

    New class - arrangedem()

--- simpledemotivators/simpledemotivators.py
+++ simpledemotivators/simpledemotivators.py
@@ -83,3 +83,25 @@ class prodemoty:
             frame = frame.convert("RGB")
             frame.save(settings.RESULT_FILENAME)
             frame.show()
+
+class arrangedem:
+        def __init__(self, text1: str, text2: str) -> str:
+                self._text1 = text1
+                self._text2 = text2
+
+        def makeImage(self, file):
+                user_img = Image.open(file).convert("RGBA")
+                (width, height) = user_img.size
+                img = Image.new('RGB', (width+250, height+240), color=('#000000'))
+                img_border = Image.new('RGB', (width+10, height+10), color=('#000000'))
+                border = ImageOps.expand(img_border, border=2, fill='#ffffff')
+                img.paste(border, (111, 96))
+                img.paste(user_img, (118, 103))
+                drawer = ImageDraw.Draw(img)
+                font_1 = ImageFont.truetype(font='times.ttf', size=50, encoding='UTF-8')
+                font_2 = ImageFont.truetype(font='arialbd.ttf', size=30, encoding='UTF-8')
+                size_1 = drawer.textsize(self._text1, font=font_1)
+                drawer.text((((width+250) - size_1[0]) / 2, ((height+160) - size_1[1])), self._text1, fill=(240, 230, 210), font=font_1)
+                size_2 = drawer.textsize(self._text2, font=font_2)
+                drawer.text((((width+250) - size_2[0]) / 2, ((height+205) - size_2[1])), self._text2, fill=(240, 230, 210), font=font_2)
+                img.save(settings.RESULT_FILENAME)
