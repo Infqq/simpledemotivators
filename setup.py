2b8de38accee3b86c87ac7a925626ac792479f03
Aleksandr <57950063+Infqq@users.noreply.github.com>
2021-01-02 21:58:33 +0400

    v1.3.0 here

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="simpledemotivators",
-    version="1.2.0",
+    version="1.3.0",
     author="infqq",
     description="Tool for easy demotivators",
     url="https://github.com/Infqq/simpledemotivators",
