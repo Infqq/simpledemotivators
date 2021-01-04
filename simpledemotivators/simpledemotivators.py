23c2bc333db868b7fbf48b167cb0f92a0d48ba27
Aleksandr <57950063+Infqq@users.noreply.github.com>
2021-01-04 20:10:23 +0400

    Update DemCreate

--- simpledemotivators/simpledemotivators.py
+++ simpledemotivators/simpledemotivators.py
@@ -12,15 +12,28 @@ class demcreate:
                 self._text2 = text2
 
         def makeImage(self, file):
+                size2 = 50
+                size3 = 30
                 img = Image.new('RGB', (1280, 1024), color=('#000000'))
                 img_border = Image.new('RGB', (1060, 720), color=('#000000'))
                 border = ImageOps.expand(img_border, border=2, fill='#ffffff')
                 user_img = Image.open(file).convert("RGBA").resize((1050, 710))
+                (width, height) = user_img.size
                 img.paste(border, (111, 96))
                 img.paste(user_img, (118, 103))
                 drawer = ImageDraw.Draw(img)
-                font_1 = ImageFont.truetype(font='times.ttf', size=60, encoding='UTF-8')
-                font_2 = ImageFont.truetype(font='arialbd.ttf', size=30, encoding='UTF-8')
+                font_1 = ImageFont.truetype(font='times.ttf', size=50, encoding='UTF-8')
+                textWidth = font_1.getsize(self._text1)[0]
+                while textWidth >= (width+250) - 20:
+                        font_1 = ImageFont.truetype(font='times.ttf', size=size2, encoding='UTF-8')
+                        textWidth = font_1.getsize(self._text1)[0]
+                        size2 -= 1
+                font_2 = ImageFont.truetype(font='times.ttf', size=30, encoding='UTF-8')
+                textWidth = font_2.getsize(self._text2)[0]
+                while textWidth >= (width+250) - 20:
+                        font_2 = ImageFont.truetype(font='times.ttf', size=size3, encoding='UTF-8')
+                        textWidth = font_2.getsize(self._text2)[0]
+                        size3 -= 1
                 size_1 = drawer.textsize(self._text1, font=font_1)
                 drawer.text(((1280 - size_1[0]) / 2, 850), self._text1, fill=(240, 230, 210), font=font_1)
                 size_2 = drawer.textsize(self._text2, font=font_2)
