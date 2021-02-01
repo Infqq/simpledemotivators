7980ff72a18b05b67d1f046a768049e19b13fee7
Aleksandr <57950063+Infqq@users.noreply.github.com>
2021-02-01 18:30:15 +0400

    new font

--- simpledemotivators/simpledemotivators.py
+++ simpledemotivators/simpledemotivators.py
@@ -29,10 +29,10 @@ class demcreate:
                         font_1 = ImageFont.truetype(font='times.ttf', size=size2, encoding='UTF-8')
                         textWidth = font_1.getsize(self._text1)[0]
                         size2 -= 1
-                font_2 = ImageFont.truetype(font='arialbd.ttf', size=size3, encoding='UTF-8')
+                font_2 = ImageFont.truetype(font='times.ttf', size=size3, encoding='UTF-8')
                 textWidth = font_2.getsize(self._text2)[0]
                 while textWidth >= (width+250) - 20:
-                        font_2 = ImageFont.truetype(font='arialbd.ttf', size=size3, encoding='UTF-8')
+                        font_2 = ImageFont.truetype(font='times.ttf', size=size3, encoding='UTF-8')
                         textWidth = font_2.getsize(self._text2)[0]
                         size3 -= 1
                 size_1 = drawer.textsize(self._text1, font=font_1)
