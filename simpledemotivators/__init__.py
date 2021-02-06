7438717fcbc50c32461b07fdedacf682d4342664
Aleksandr <57950063+Infqq@users.noreply.github.com>
2021-02-06 17:21:30 +0400

    + quote

--- simpledemotivators/__init__.py
+++ simpledemotivators/__init__.py
@@ -3,4 +3,5 @@ from .simpledemotivators import demcreate
 from .simpledemotivators import prodemoty
 from .simpledemotivators import arrangedem
 from .simpledemotivators import settings
+from .simpledemotivators import quote
 from .simpledemotivators import *
