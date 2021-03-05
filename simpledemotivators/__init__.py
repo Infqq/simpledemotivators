4822794f2f8b04ae42236112ed0cc5ce24268a1f
Aleksandr <57950063+Infqq@users.noreply.github.com>
2021-03-05 18:42:38 +0400

    lil delete

--- simpledemotivators/__init__.py
+++ simpledemotivators/__init__.py
@@ -1,6 +1,5 @@
 import simpledemotivators
 from .simpledemotivators import demcreate
-from .simpledemotivators import prodemoty
 from .simpledemotivators import arrangedem
 from .simpledemotivators import settings
 from .simpledemotivators import quote
