4ca25eca3b71b22410e0df7bbeab5fd5ce93e29d
Aleksandr <57950063+Infqq@users.noreply.github.com>
2021-03-22 10:25:11 +0400

    Улучшена читабельность кода, кастомизация

--- simpledemotivators/simpledemotivators.py
+++ simpledemotivators/simpledemotivators.py
@@ -7,7 +7,7 @@ version = requests.get(
         'https://raw.githubusercontent.com/infqq/simpledemotivators/master/version.txt'
         ).text.splitlines()
 
-if version[0] != '1.5.0':
+if version[0] != '1.6.0':
         print(
                 f'[SimpleDemotivators] Данная версия библиотеки устарела, обновитесь до v{version[0]} с GitHub\nИзменения: {version[1]}')
 else:
@@ -16,7 +16,9 @@ else:
 
 
 class demcreate:
-        def __init__(self, text1: str, text2: str) -> str:
+        def __init__(
+                self, text1, text2) -> str:
+                
                 self._text1 = text1
                 self._text2 = text2
 
@@ -68,7 +70,9 @@ class demcreate:
                 
                 img.save(RESULT_FILENAME)
 
-        def setline(self, text, RESULT_FILENAME='demresult.jpg', fonttext='times.ttf'): # Лучше не использовать
+        def setline(
+                self, text, RESULT_FILENAME='demresult.jpg', fonttext='times.ttf'): # Лучше не использовать
+
                 if len(text) > 12:
                         photo1 = Image.open(self._file)
                         (width, height) = photo1.size
@@ -89,45 +93,68 @@ class demcreate:
                         photo1.save(RESULT_FILENAME)
 
 class arrangedem:
-        def __init__(self, text1: str, text2: str) -> str:
+        def __init__(
+                self, text1, text2) -> str:
+                
                 self._text1 = text1
                 self._text2 = text2
 
-        def makeImage(self, file, RESULT_FILENAME='demresult.jpg'):
-                size2 = 80
-                size3 = 60
+        def makeImage(
+                self, file, RESULT_FILENAME='demresult.jpg', colortext = 'white',
+                colorfill = 'black', fonttext='times.ttf', size2 = 80, size3 = 60
+                ):
+
+                """Создаем основу демотиватора
+
+                Регулируем края демотиватора и вставляем изображение
+                
+                """
                 user_img = Image.open(file).convert("RGBA")
                 (width, height) = user_img.size
-                img = Image.new('RGB', (width+250, height+240), color=('#000000'))
+                img = Image.new('RGB', (width+250, height+240), color=colorfill)
                 img_border = Image.new('RGB', (width+10, height+10), color=('#000000'))
                 border = ImageOps.expand(img_border, border=2, fill='#ffffff')
                 img.paste(border, (111, 96))
                 img.paste(user_img, (118, 103))
                 drawer = ImageDraw.Draw(img)
-                font_1 = ImageFont.truetype(font='times.ttf', size=50, encoding='UTF-8')
+
+                """Определяем оптимальный размер шрифта"""
+                
+                font_1 = ImageFont.truetype(font=fonttext, size=50, encoding='UTF-8')
                 textWidth = font_1.getsize(self._text1)[0]
+                
                 while textWidth >= (width+250) - 20:
-                        font_1 = ImageFont.truetype(font='times.ttf', size=size2, encoding='UTF-8')
+                        font_1 = ImageFont.truetype(font=fonttext, size=size2, encoding='UTF-8')
                         textWidth = font_1.getsize(self._text1)[0]
                         size2 -= 1
-                font_2 = ImageFont.truetype(font='times.ttf', size=30, encoding='UTF-8')
+
+                font_2 = ImageFont.truetype(font=fonttext, size=30, encoding='UTF-8')
                 textWidth = font_2.getsize(self._text2)[0]
+                
                 while textWidth >= (width+250) - 20:
-                        font_2 = ImageFont.truetype(font='times.ttf', size=size3, encoding='UTF-8')
+                        font_2 = ImageFont.truetype(font=fonttext, size=size3, encoding='UTF-8')
                         textWidth = font_2.getsize(self._text2)[0]
                         size3 -= 1
+
+                """Перемещаем текст в демотиватор"""
+                
                 size_1 = drawer.textsize(self._text1, font=font_1)
-                drawer.text((((width+250) - size_1[0]) / 2, ((height+170) - size_1[1])), self._text1, fill=(240, 230, 210), font=font_1)
+                drawer.text((((width+250) - size_1[0]) / 2, ((height+170) - size_1[1])), self._text1, fill=colortext, font=font_1)
                 size_2 = drawer.textsize(self._text2, font=font_2)
-                drawer.text((((width+250) - size_2[0]) / 2, ((height+215) - size_2[1])), self._text2, fill=(240, 230, 210), font=font_2)
+                drawer.text((((width+250) - size_2[0]) / 2, ((height+215) - size_2[1])), self._text2, fill=colortext, font=font_2)
+                
                 img.save(RESULT_FILENAME)
 
 class quote:
-        def __init__(self, text: str, name: str) -> str:
+        def __init__(
+                self, text, name) -> str:
+                
                 self._text = text
                 self._name = name
 
-        def get(self, file, RESULT_FILENAME='qresult.jpg'):
+        def get(
+                self, file, RESULT_FILENAME='qresult.jpg'):
+                
                 text = ''
                 lines = textwrap.wrap('"' + self._text + '"', width=24)
 
