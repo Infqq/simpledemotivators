ae8d59c29ca94ba8a2eea7d89488d03397d49a3d
Aleksandr <57950063+Infqq@users.noreply.github.com>
2021-04-16 10:03:02 +0400

    Update __init__.py

--- simpledemotivators/__init__.py
+++ simpledemotivators/__init__.py
@@ -1,7 +1,6 @@
 import requests
 
-from .Demcreate import Demcreate
-from .Arrangedem import Arrangedem
+from .Demotivator import Demotivator
 from .Quote import Quote
 from .Text_gen import Text_gen
 
@@ -9,7 +8,7 @@ version = requests.get(
         'https://raw.githubusercontent.com/infqq/simpledemotivators/master/simpledemotivators/version.txt'
         ).text.splitlines()
 
-if version[0] != '1.8.0':
+if version[0] != '2.0.0':
         print(
                 f'[SimpleDemotivators] Данная версия библиотеки устарела, обновитесь до v{version[0]} с GitHub\nИзменения: {version[1]}')
 else:
@@ -17,8 +16,7 @@ else:
                 f'SimpleDemotivators v{version[0]} started, version actual.')
 
 __all__ = (
-        'Demcreate',
-        'Arrangedem',
+        'Demotivator',
         'Quote',
         'Text_gen'
 )
