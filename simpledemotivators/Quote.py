7a56e9fe2e936cd72b048790bf4f2cf99a6263b0
Aleksandr <57950063+Infqq@users.noreply.github.com>
2021-09-19 10:59:10 +0400

    Настройка headline + некоторые изменения

--- simpledemotivators/Quote.py
+++ simpledemotivators/Quote.py
@@ -12,8 +12,8 @@ class Quote:
         self._name = name
 
     def get(
-            self, file, RESULT_FILENAME='qresult.jpg', url=False, headline_font='times.ttf', headline_size=60,
-            headline_text='Цитаты великих людей', name_font = 'times.ttf', name_size=40, text_font='arialbd.ttf', text_size=40
+            self, file, RESULT_FILENAME='qresult.jpg', url=False, headline_font='verdana.ttf', headline_size=50,
+            headline_text='Цитаты великих людей', name_font = 'times.ttf', name_size=40, text_font='arial.ttf', text_size=40
             ):
 
         text = ''
@@ -33,10 +33,11 @@ class Quote:
         font_2 = ImageFont.truetype(font=headline_font, size=headline_size, encoding='UTF-8')
         font_3 = ImageFont.truetype(font=name_font, size=name_size, encoding='UTF-8')
 
-        drawer.text((529, 90), text, fill='white', font=font_1)
+        size_headline = drawer.textsize(headline_text, font=font_2)
 
-        drawer.text((529, 460), '© ' + self._name, fill='white', font=font_3)
-        drawer.text((270, 5), headline_text, fill='white', font=font_2)
+        drawer.text((529, 120), text, fill='white', font=font_1)
+        drawer.text((529, 490), '© ' + self._name, fill='white', font=font_3)
+        drawer.text(((1155 - size_headline[0]) / 2, 25), headline_text, fill='white', font=font_2)
 
         if url:
             p = requests.get(file)
@@ -47,7 +48,7 @@ class Quote:
             file = 'quote_picture.jpg'
         
         img = Image.open(file).convert("RGBA").resize((400, 400))
-        user_img.paste(img, (100, 100))
+        user_img.paste(img, (100, 130))
 
         user_img.save(RESULT_FILENAME)
         
