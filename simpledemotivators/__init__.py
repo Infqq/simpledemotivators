8bf659dd7f05c6de3d5c61b8adaab8c0ee3d8176
Aleksandr <57950063+Infqq@users.noreply.github.com>
2021-10-30 20:12:12 +0400

    refactor

--- simpledemotivators/__init__.py
+++ simpledemotivators/__init__.py
@@ -5,18 +5,18 @@ from .Quote import Quote
 from .Text_gen import Text_gen
 
 try:
-        version = requests.get(
-                'https://raw.githubusercontent.com/infqq/simpledemotivators/master/simpledemotivators/version.txt'
-                ).text.splitlines()
+    version = requests.get(
+        'https://raw.githubusercontent.com/infqq/simpledemotivators/master/simpledemotivators/version.txt'
+    ).text.splitlines()
 
-        if version[0] != '2.0.1':
-                print(
-                        f'[SimpleDemotivators] Данная версия библиотеки устарела, обновитесь до v{version[0]} с GitHub\nИзменения: {version[1]}')
+    if version[0] != '2.0.1':
+        print(
+            f'[SimpleDemotivators] Данная версия библиотеки устарела, обновитесь до v{version[0]} с GitHub\n',
+            'Изменения: {version[1]}')
 except:
-        print('[SimpleDemotivators] Не удалось проверить версию библиотеки на актуальность')
-        
+    print('[SimpleDemotivators] Не удалось проверить версию библиотеки на актуальность')
+
 __all__ = (
-        'Demotivator',
-        'Quote',
-        'Text_gen'
+    'Demotivator',
+    'Quote'
 )
