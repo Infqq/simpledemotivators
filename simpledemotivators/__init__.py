d23ac27072071d9ac2311ef34b16192d18daf8c0
BlitGaming <blitgaming1007@mail.ru>
2021-10-30 23:52:28 +0700

    fix __init__.py refactor Demotivator.py

--- simpledemotivators/__init__.py
+++ simpledemotivators/__init__.py
@@ -2,7 +2,6 @@ import requests
 
 from .Demotivator import Demotivator
 from .Quote import Quote
-from .Text_gen import Text_gen
 
 try:
     version = requests.get(
@@ -13,7 +12,7 @@ try:
         print(
             f'[SimpleDemotivators] Данная версия библиотеки устарела, обновитесь до v{version[0]} с GitHub\n',
             'Изменения: {version[1]}')
-except:
+except requests.exceptions.RequestException:
     print('[SimpleDemotivators] Не удалось проверить версию библиотеки на актуальность')
 
 __all__ = (
