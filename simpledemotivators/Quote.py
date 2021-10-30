93bc539d9446f465e41152d54eb71472dc614f8d
BlitGaming <blitgaming1007@mail.ru>
2021-10-30 23:56:06 +0700

    lite refactor Quote.py

--- simpledemotivators/Quote.py
+++ simpledemotivators/Quote.py
@@ -6,18 +6,20 @@ import os
 
 class Quote:
     def __init__(self, text, name):
-
         self._text = text
         self._name = name
 
-    def get(self, file, result_filename='qresult.jpg', url=False, headline_font='verdana.ttf', headline_size=50,
-            headline_text='Цитаты великих людей', name_font='times.ttf', name_size=40, text_font='arial.ttf',
-            text_size=40):
+    def get(self, file, result_filename='qresult.jpg', use_url=False,
+            headline_font='verdana.ttf', headline_size=50,
+            headline_text='Цитаты великих людей', name_font='times.ttf',
+            name_size=40, text_font='arial.ttf', text_size=40) \
+            -> bool:  # Returns True if method executed successfully
 
         text = ''
         lines = textwrap.wrap(self._text, width=24)
 
-        for i in lines: text = text + i + '\n'
+        for i in lines:
+            text = text + i + '\n'
 
         if len(text.splitlines()) > 9:
             lines = text.splitlines()[0:9]
@@ -25,7 +27,7 @@ class Quote:
             for i in lines:
                 text = text + i + '\n'
 
-        user_img = Image.new('RGB', (1155, 600), color=('#000000'))
+        user_img = Image.new('RGB', (1155, 600), color='#000000')
 
         drawer = ImageDraw.Draw(user_img)
         font_1 = ImageFont.truetype(font=text_font, size=text_size, encoding='UTF-8')
@@ -38,7 +40,7 @@ class Quote:
         drawer.text((529, 490), '© ' + self._name, fill='white', font=font_3)
         drawer.text(((1155 - size_headline[0]) / 2, 25), headline_text, fill='white', font=font_2)
 
-        if url:
+        if use_url:
             p = requests.get(file)
             out = open(r'quote_picture.jpg', "wb")
             out.write(p.content)
@@ -51,5 +53,7 @@ class Quote:
 
         user_img.save(result_filename)
 
-        if url:
+        if use_url:
             os.remove('quote_picture.jpg')
+
+        return True
