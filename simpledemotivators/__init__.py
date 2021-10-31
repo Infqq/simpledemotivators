c8a8e1bbef019b97927e4feb5b9b7ab8260c13b8
BlitGaming <blitgaming1007@mail.ru>
2021-10-31 17:49:14 +0700

    fix version detection

--- simpledemotivators/__init__.py
+++ simpledemotivators/__init__.py
@@ -4,14 +4,18 @@ from .Demotivator import Demotivator
 from .Quote import Quote
 
 try:
+    is_dev = True
+
     version = requests.get(
         'https://raw.githubusercontent.com/infqq/simpledemotivators/master/simpledemotivators/version.txt'
     ).text.splitlines()
 
-    if version[0] != '2.1.0':
-        print(
-            f'[SimpleDemotivators] Данная версия библиотеки устарела, обновитесь до v{version[0]} с GitHub\n',
-            'Изменения: {version[1]}')
+    if is_dev:
+        print("[SimpleDemotivators] Вы используете версию для разработчиков!",
+              "\nПожалуйста не используйте эту версию при разработке реального проекта.")
+    elif version[0] != '2.1.0':
+        print(f'[SimpleDemotivators] Данная версия библиотеки устарела, обновитесь до v{version[0]} с GitHub',
+              f'\nИзменения: {version[1]}')
 except requests.exceptions.RequestException:
     print('[SimpleDemotivators] Не удалось проверить версию библиотеки на актуальность')
 
