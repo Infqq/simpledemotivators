f9d72f5e37a5abf315a40962b082e3980735a001
BlitGaming <blitgaming1007@mail.ru>
2022-04-15 19:17:54 +0700

    Prepare release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="simpledemotivators",
-    version="2.1.0",
+    version="2.1.1",
     author="infqq",
     description="Tool for easy demotivators",
     url="https://github.com/Infqq/simpledemotivators",
